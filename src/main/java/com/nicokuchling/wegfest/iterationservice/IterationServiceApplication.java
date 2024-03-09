2a74b024b9f61c99b367654107a4d38f1a9cc356
Nico Kuchling <nico.kuchling@outlook.de>
2024-03-09 14:33:21 +0100

    Iteration-service initial commit

---
+++ src/main/java/com/nicokuchling/wegfest/iterationservice/IterationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nicokuchling.wegfest.iterationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IterationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicokuchling/wegfest/iterationservice/IterationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicokuchling.wegfest.iterationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
